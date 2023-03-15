b12228f974f2aa73b6a9d81698cd9953c33c935e
Ethan Dickson <ethanndickson@gmail.com>
2023-03-15 18:31:52 +1100

    test: added test framework stubs

---
+++ common/tests/objects.rs
@@ -0,0 +1,28 @@
+use common::xsd::EndDeviceControl;
+
+#[test]
+fn edc_deserialize() {
+    let edc = EndDeviceControl {
+        appliance_load_reduction: todo!(),
+        device_category: todo!(),
+        dr_program_mandatory: todo!(),
+        duty_cycle: todo!(),
+        load_shift_forward: todo!(),
+        offset: todo!(),
+        override_duration: todo!(),
+        set_point: todo!(),
+        target_reduction: todo!(),
+        randomize_duration: todo!(),
+        randomize_start: todo!(),
+        creation_time: todo!(),
+        event_status: todo!(),
+        interval: todo!(),
+        m_rid: todo!(),
+        description: todo!(),
+        version: todo!(),
+        subscribable: todo!(),
+        reply_to: todo!(),
+        response_required: todo!(),
+        href: todo!(),
+    };
+}
