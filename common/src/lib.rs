02bb3124897ac4d030c4879e275bef40d3833340
Ethan Dickson <ethanndickson@gmail.com>
2023-11-04 16:08:18 +1100

    chore: cleaned err handling

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -12,26 +12,22 @@ pub mod traits;
 
 /// Given an IEEE 2030.5 data type, serialize it into an XML string
 pub fn serialize<R: SEType>(resource: &R) -> Result<String> {
-    log::debug!("Serialize: {}", R::name());
-    let res = panic::catch_unwind(|| to_string(resource).map_err(|e| anyhow!(e)));
-    match res {
-        Ok(res) => res,
-        Err(_) => Err(anyhow!(
-            "Fatal Serializer Error: Unable to Serialize {}",
+    log::debug!("Serializing: {}", R::name());
+    panic::catch_unwind(|| to_string(resource).map_err(|e| anyhow!(e))).map_err(|_| {
+        anyhow!(
+            "Fatal Serializer Error: Unable to Serialize {} due to a panic",
             R::name()
-        )),
-    }
+        )
+    })?
 }
 
 /// Given a string representing an IEEE 2030.5 data type, deserialize into it the inferred type
 pub fn deserialize<R: SEType>(resource: &str) -> Result<R> {
-    log::debug!("Deserialize: {}", R::name());
-    let res = panic::catch_unwind(|| from_str::<R>(resource).map_err(|e| anyhow!(e)));
-    match res {
-        Ok(res) => res,
-        Err(_) => Err(anyhow!(
-            "Fatal XML Parser Error: Unable to Deserialize {}",
+    log::debug!("Deserializing: {}", R::name());
+    panic::catch_unwind(|| from_str::<R>(resource).map_err(|e| anyhow!(e))).map_err(|_| {
+        anyhow!(
+            "Fatal XML Parser Error: Unable to Deserialize {} due to a panic",
             R::name()
-        )),
-    }
+        )
+    })?
 }
